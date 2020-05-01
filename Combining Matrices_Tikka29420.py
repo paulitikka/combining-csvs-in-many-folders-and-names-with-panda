0257e2b44bc52f043c4f071ae266e7d0feda4b5d
paulitikka <30927365+paulitikka@users.noreply.github.com>
2020-05-01 19:10:02 +0200

    Streamlined the codes

--- Combining Matrices_Tikka29420.py
+++ Combining Matrices_Tikka29420.py
@@ -37,7 +37,7 @@ for filename in all_files:
     li.append(df)
 
 frame = pd.concat(li, axis=0, ignore_index=True)
-#%%
+#%
 resulta=[]
 resulta=['Journal Name','Title of Article', \
                'Writers of Article', \
@@ -51,12 +51,8 @@ resulta=['Journal Name','Title of Article', \
                'Link to All Reviews',
                'Link to PDF of Review']
 frame = frame[resulta] 
-#%%
+#%
 frame.rename(columns={'Link to PDF of Review':'Link to PDF of Reviewer'}, inplace=True)
-#%%
-import pandas as pd
-import glob
-
 path = r'C:\python\all_csvs\bmc' # use your path
 all_files = glob.glob(path + "/*.csv")
 
@@ -67,7 +63,6 @@ for filename in all_files:
     li.append(df)
 
 frame1 = pd.concat(li, axis=0, ignore_index=True)
-#%%
 resulta=[]
 resulta=['Journal Name','Title of Article', \
                'Writers of Article', \
@@ -81,10 +76,6 @@ resulta=['Journal Name','Title of Article', \
                'Link to All Reviews',
                'Link to PDF of Reviewer']
 frame1 = frame1[resulta] 
-#%%
-import pandas as pd
-import glob
-
 path = r'C:\python\all_csvs\plos' # use your path
 all_files = glob.glob(path + "/*.csv")
 
@@ -95,7 +86,7 @@ for filename in all_files:
     li.append(df)
 
 frame2 = pd.concat(li, axis=0, ignore_index=True)
-    #%%PLOS is tricky:
+    #%LOS is tricky:
 resulta=[]
 resulta=['Journal Name','Title of Article', \
                'Writers of Article', \
@@ -109,7 +100,7 @@ resulta=['Journal Name','Title of Article', \
                'Link to All Reviews',
                'Link to PDF of Reviewer']
 frame2 = frame2[resulta] 
-#%% Some info regarding:   
+#% Some info regarding:   
 #https://cmdlinetips.com/2018/03/how-to-change-column-names-and-row-indexes-in-pandas/
 #https://kite.com/python/answers/how-to-reorder-columns-in-a-pandas-dataframe-in-python
 #https://kite.com/python/answers/how-to-sum-rows-of-a-pandas-dataframe-in-python
@@ -117,5 +108,31 @@ frame2 = frame2[resulta]
 #%https://datascience.stackexchange.com/questions/41448/how-to-rename-columns-that-have-the-same-name
 tot_res=[]
 tot_res=pd.concat([frame,frame1,frame2], axis=0, ignore_index=True)
-tot_res.to_csv('all_journals_tikka29420.csv', index=False, na_rep='NA')
+tot_res.to_csv('all_journals_tikka1520.csv', index=False, na_rep='NA')
 #https://stackoverflow.com/questions/50890989/pandas-changing-the-format-of-nan-values-when-saving-to-csv
+
+#%Curating word counts folder:
+tot_res=[]
+tot_res = pd.read_csv('all_journals_tikka1520.csv')
+#%
+tot_res=tot_res[tot_res['Review Word Count']!=0]
+#%
+tot_res=tot_res[tot_res['Review Word Count']<=5200]
+
+tot_res=tot_res.replace(np.nan, 'NA', regex=True)
+#%Replacing names
+#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.replace.html
+tot_res=tot_res.replace('Journal Reviewer', 'NA')
+tot_res=tot_res.replace('Journal Reviewer Title', 'NA')
+tot_res=tot_res.replace('xx', 'NA')
+tot_res=tot_res.replace('Journal Institution', 'NA')
+#https://stackoverflow.com/questions/26837998/pandas-replace-nan-with-blank-empty-string
+#%%
+#%The absolute index as written comes with loc:
+#nati=tot_res.loc[319,:]
+#tot_res=tot_res[tot_res.index!=319]
+tot_res.to_csv('all_journals_tikka1520_ok.csv', index=False, na_rep='NA')
+#%%
+tot_res2=tot_res[tot_res['Review Word Count']>=20]
+#%%
+tot_res.to_csv('all_journals_tikka1520_ok2.csv', index=False, na_rep='NA')
