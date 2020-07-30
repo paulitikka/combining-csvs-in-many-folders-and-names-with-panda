2c3a0c1be3fd5efdae31ce05d633ca9e582344a6
Pauli Tikka <30927365+paulitikka@users.noreply.github.com>
2020-07-30 10:53:03 +0200

    Update Combining Matrices_Tikka29420.py

--- Combining Matrices_Tikka29420.py
+++ Combining Matrices_Tikka29420.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 15 13:11:14 2020, updated 17.7.20
+Created on Wed Apr 15 13:11:14 2020
 
 @author: pauli
 """
@@ -148,15 +148,10 @@ totaa=[frame1, frame, frame2]
 tot_res = pd.concat(totaa, axis=0, ignore_index=True, sort=False)
 #tot_res.to_csv('all_journals_tikka1520.csv', index=False, na_rep='NA')
 #https://stackoverflow.com/questions/50890989/pandas-changing-the-format-of-nan-values-when-saving-to-csv
-
-#%Curating word counts folder:
-#tot_res=[]
+#%Curating from older word counts folder:
 #tot_res = pd.read_csv('all_journals_tikka1520.csv')
-#%%
 #tot_res=tot_res[tot_res['Review Word Count']!=0]
-#%
 #tot_res=tot_res[tot_res['Review Word Count']<=5200]
-
 tot_res=tot_res.replace(np.nan, 'NA', regex=True)
 #%Replacing names
 #https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.replace.html
@@ -166,10 +161,7 @@ tot_res=tot_res.replace('xx', 'NA')
 tot_res=tot_res.replace('Journal Institution', 'NA')
 tot_res['Attachments']=tot_res['Attachments'].replace('nan', 'no', regex=True)
 tot_res['Page Count']=tot_res['Page Count'].replace('nan', 'NA', regex=True)
-#%
 #tot_res['Reviewer Name']=tot_res['Reviewer Name'].replace('', 'NA', regex=True)
-#%
-#%
 tot_res['Review Word Count']=tot_res['Review Word Count'].replace('NA', 0, regex=True)
 tot_res['Review Word Count'] = tot_res['Review Word Count'].astype(int) 
 #In some columns you need to change the type from int/else to string so that when you
@@ -177,21 +169,18 @@ tot_res['Review Word Count'] = tot_res['Review Word Count'].astype(int)
 #https://stackoverflow.com/questions/22005911/convert-columns-to-string-in-pandas
 #df['Reviewer\'s Title'] = df['Reviewer\'s Title'].astype(str) 
 #https://stackoverflow.com/questions/26837998/pandas-replace-nan-with-blank-empty-string
-#%
 #%The absolute index as written comes with loc:
 #nati=tot_res.loc[319,:]
-#tot_res=tot_res[tot_res.index!==6093]
-#%%
+#tot_res=tot_res[tot_res.index!=6093]
 tot_res=tot_res[tot_res['Review Word Count']!=0]
-#%
 tot_res=tot_res[tot_res['Review Word Count']!=1]
-#%%
-tot_res=tot_res[tot_res['Reviewer Name']!='Qin Nin']
+tot_res=tot_res[tot_res['Reviewer Name']!='Kirsti Uusi-Ras']
 #tot_res=tot_res[tot_res['Link to PDF of Reviewer']!='NA']
 #tot_res['Review Word Count'] = tot_res['Review Word Count'].astype(int) 
-#%%
+#%%Removing small ro big values from tot_res that are not ok, first open them and sorthem to separate file,
+#and open them with other function:
 import pandas as pd
-f =  pd.read_csv('totaxs.txt', header=None)
+f =  pd.read_csv('totaxs2.txt', header=None)
 #https://stackoverflow.com/questions/21546739/load-data-from-txt-with-pandas
 #https://www.quora.com/How-do-I-open-a-URL-in-Google-Chrome-in-new-tab-using-Python
 import webbrowser
@@ -199,68 +188,29 @@ for i in range(len(f)):
     url = f.iloc[i][0]
     webbrowser.open_new_tab(url)
 #%%
-tot_res=tot_res[tot_res.index!=4826]
-#%%
-#%%
+#tot_res=tot_res[tot_res.index!=4826]
 #tot_res=tot_res.replace(np.nan, 'NA', regex=True)
-tot_res.to_csv('all_journals_tikka16720_ok1.csv', index=True, na_rep='NA')
-all_journals_tikka16720_ok1
-#%%
-tot_res = pd.read_csv('all_journals_tikka16720_ok1.csv', index_col=None, header=0)
+tot_res.to_csv('all_journals_tikka23720_ok1.csv', index=False, na_rep='NA') #index can be also true
+#tot_res = pd.read_csv('all_journals_tikka23720_ok1.csv', index_col=None, header=0)
 #tot_res2=tot_res[tot_res['Review Word Count']>=20]
-#%%
-tot_res.to_csv('all_journals_tikka17720_ok2a.csv', index=False, na_rep='NA')
-#%%
+#%%The random check matric
 import random
 test=[]
-for i in range(0,8):
+for i in range(0,50):
     test.append(int(len(tot_res)*random.random()))
-    #%
 test=np.sort(test)
 testa=pd.DataFrame(test)
-#%
-#testa.to_csv('rnd_chk_tikka7720_ok.csv', index=False, na_rep='NA')
-#%
-#testa = pd.read_csv('rnd_chk_tikka18620_ok.csv', index_col=None, header=0)
-#%
-#1)428: 
 natax=[]
 for i in range(len(testa)):
     natax.append(tot_res.loc[testa.iloc[i,0],['Review Word Count', 'Link to PDF of Reviewer', 'Reviewer Name']])
-    #%
-#natan=[]
-#for i in range(len(testa)):
-#    natan.append(tot_res.loc[testa.iloc[i,0],['Reviewer Name']])
-    #%
 natax = pd.concat(natax, axis=1, ignore_index=True, sort=False)
 natax=natax.T
-#%%
-    #%
-#tot_res.loc[testa.iloc[1,0],['Review Word Count', 'Link to PDF of Reviewer']]
-#%
-#nataa = pd.concat(nata, axis=1, ignore_index=True, sort=False)
-#nataa=nataa.T
 natax["check"]='nan'
-#%
 natax=pd.DataFrame(natax)
-#%
-#nata.iloc[:,0]= nata.iloc[:,0].astype(float) 
-#%
-#natax["Reviewer Name"]=natan['Reviewer Name']
-#%
 natax.iloc[:,0]= natax.iloc[:,0].astype(float)  
 natax.iloc[:,3]= natax.iloc[:,3].astype(float)   
-#%%
-nata=pd.concat([nata,natax])
-#%% 
-nata.to_csv('rnd_chk_ongoing_tikka16720_ok3.csv', index=False, na_rep='NA') 
- #%%
-nata=nata[nata['Reviewer Name']!='Susan Janso'] 
-#%%
-nata=nata[nata.index!=48]
-#%%
+#%%Here you take all the links from 'natax' test function:
 import pandas as pd
-#%%
 f =  pd.read_csv('totaxl.txt', header=None)
 #https://stackoverflow.com/questions/21546739/load-data-from-txt-with-pandas
 #https://www.quora.com/How-do-I-open-a-URL-in-Google-Chrome-in-new-tab-using-Python
@@ -269,103 +219,60 @@ for i in range(len(f)):
     url = f.iloc[i][0]
     webbrowser.open_new_tab(url)  
 #%%
-nata = pd.read_csv('rnd_chk_ongoing_tikka16720_ok3.csv', index_col=None, header=0)
-#%%Results
-nata.iloc[:,3]= nata.iloc[:,3].astype(float) 
-np.mean(nata.iloc[:,3])
-#%%
-np.std(nata.iloc[:,3])
-#%%
-nata.iloc[:,0]= nata.iloc[:,0].astype(float) 
-np.mean(nata.iloc[:,0])
-#%%
-np.std(nata.iloc[:,0])
-
-rng=tot_res2['Review Word Count']
-#%
-import matplotlib.pyplot as plt
-#rng = np.random.RandomState(10)  # deterministic random data
-a = np.hstack(rng,)
-_ = plt.hist(a, bins='auto')  # arguments are passed to np.histogram
-plt.title("Histogram with 'auto' bins")
-Text(0.5, 1.0, "Histogram with 'auto' bins")
-plt.show()
-#%%
-#%%
-a=[3928,
-4568,
-5589,
-...,3928]
-#%%
-#tot_res=tot_res[tot_res.index!=a]
-tot_res=tot_res.drop(oo, axis=0)
-#%%
-
-#%%
+natax.to_csv('rnd_chk_ongoing_tikka16720_ok3.csv', index=False, na_rep='NA') #redo below?
+#nata=nata[nata['Reviewer Name']!='Susan Janso'] 
+#nata=nata[nata.index!=48]
+#nata = pd.read_csv('rnd_chk_ongoing_tikka16720_ok3.csv', index_col=None, header=0)
+#%Results
+#%Your codes' word counts:
+natax.iloc[:,0]= natax.iloc[:,0].astype(float) 
+a=round(np.mean(natax.iloc[:,0]),1)
+b=round(np.std(natax.iloc[:,0]),1)
+#%The Word program counts:
+natax.iloc[:,3]= natax.iloc[:,3].astype(float) 
+c=round(np.mean(natax.iloc[:,3]),1)
+d=round(np.std(natax.iloc[:,3]),1)
+#%%Deleting rows or other no good from data:
 file_handle = open('noss.txt', 'r')
-#%
 lines_list = file_handle.readlines()
-#%
-cols= (int(val) for val in lines_list[0].split())
-#%
 # Do a double-nested list comprehension to get the rest of the data into your matrix
 #https://stackoverflow.com/questions/6583573/how-to-read-numbers-from-file-in-python
-my_data = [[int(val) for val in line.split()] for line in lines_list[1:]]
-#%
 oo=[]
-for i in range(len(my_data)):
-    oo.append(my_data[i][0])
-    #%%
-
-tot_res['Reviewer Name']
-'Reviewing Date'
-#%%
-idx = pd.Index(tot_res['Reviewer Name'])
-axan=idx.duplicated()
-#%%
-idx2 = pd.Index(tot_res['Reviewing Date'])
-daxan=idx2.duplicated()
-#%%
-idx3 = pd.Index(tot_res['Date of Publication'])
-paxan=idx3.duplicated()
-#%%
-idx4 = pd.Index(tot_res['Link to PDF of Reviewer'])
-taxan=idx4.duplicated()
-#%%
-jaksan=[]
-for i in range(len(daxan)):
-    if daxan[i]==True and daxan[i]==True and paxan[i]==True and taxan[i]==True:
-        jaksan.append(i)
-#%%
+for i in range(len(lines_list)):
+    oo.append(int(lines_list[i]))
+#a=[3928,4568]
+#%tot_res=tot_res[tot_res.index!=a] #alternative way to delete bad rows
+tot_res=tot_res.drop(oo, axis=0)
+#%%If you have the names of not oks, you cand do as follows:
+data=[]
+data = pd.read_csv('oos.txt', header = None)
+xaa=[]
+for i in tot_res.index:
+    for j in range(len(data)):
+        if data.loc[j][0] in tot_res.loc[i,'Reviewer Name']: #and tot_res.loc[i,'Reviewer Name']!='nan':
+            xaa.append(i)   
+tot_res=tot_res.drop(xaa, axis=0)           
 tot_res2=tot_res
 tot_res2=tot_res2.reset_index(drop=True)
+#Removing doubles..
 utta=[]
 for i in range(len(tot_res2)-1):
     if tot_res2['Reviewer Name'].loc[i]==tot_res2['Reviewer Name'].loc[i+1] and \
     tot_res2['Reviewing Date'].loc[i]==tot_res2['Reviewing Date'].loc[i+1] and \
-    tot_res2['Date of Publication'].loc[i]==tot_res2['Date of Publication'].loc[i+1] and \
-    tot_res2['Link to PDF of Reviewer'].loc[i]==tot_res2['Link to PDF of Reviewer'].loc[i+1] and \
-    tot_res2['Review Word Count'].loc[i]==tot_res2['Review Word Count'].loc[i+1]:
+    tot_res2['Reviewer Name'].loc[i]!='NA':
         utta.append(i)
-        #%%
 idxa = pd.Index(utta)
-axn=idxa.duplicated()
-#axn0
-#%%
 tak=[]
 for i in range(len(idxa)-1):
     if idxa[i]+1==idxa[i+1]:
         tak.append(idxa[i])
-        #%%ok
+#%ok
 tak2=[]
 for i in range(len(tak)-1):
     if tak[i]+1==tak[i+1]:
         tak2.append(tak[i])        
-#%%
-tot_res2=tot_res2.drop(tak,axis=0)
-#%%
-tot_res2.to_csv('all_journals_tikka17720_ok4a.csv', index=False, na_rep='NA')
-#%%
-np.std(rng)
-#%%
-#tot_res2=pd.read_csv('all_journals_tikka17720_ok3a.csv', index_col=None, header=0)
+tot_res2=tot_res2.drop(utta,axis=0) #check also tak/2
+#%Saving the values
+tot_res2.to_csv('all_journals_tikka23720_ok.csv', index=False, na_rep='NA')
+#or loading:
+#tot_res2=pd.read_csv('all_journals_tikka21720_ok.csv', index_col=None, header=0)
